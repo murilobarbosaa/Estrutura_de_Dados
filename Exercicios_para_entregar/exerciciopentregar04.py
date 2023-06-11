0997d9ad40ab07b1b727795c5cfadb6b67b16ffc
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-11 15:49:06 -0300

    Delete exerciciopentregar04.py

--- Exercicios_para_entregar/exerciciopentregar04.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Digite um número inteiro entre 1 e 10: "))
-
-if numero < 1 or numero > 10:
-    print("Número inválido. Por favor, escolha um número entre 1 e 10.")
-else:
-    print(f"Tabuada do número {numero}:")
-    for i in range(1, 11):
-        resultado = numero * i
-        print(f"{numero} x {i} = {resultado}")
